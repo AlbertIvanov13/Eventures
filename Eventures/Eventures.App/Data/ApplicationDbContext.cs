e7978b94cc238c7d4c39dbe338df9dd6bf94506c
AlbertIvanov13 <albertivanov187@gmail.com>
2022-02-17 14:04:08 +0200

    UploadProject

---
+++ Eventures/Eventures.App/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Eventures.App.Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eventures.App.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<EventuresUser>
+    {
+        public DbSet<Event> Events { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            this.Database.EnsureCreated();
+        }
+    }
+}
