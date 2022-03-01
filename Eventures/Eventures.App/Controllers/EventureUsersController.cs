d9ca33c00e504bf5523982a0dfdec06090714bd5
AlbertIvanov13 <albertivanov187@gmail.com>
2022-03-01 09:25:40 +0200

    Upload project

--- Eventures/Eventures.App/Controllers/EventureUsersController.cs
+++ Eventures/Eventures.App/Controllers/EventureUsersController.cs
@@ -1,4 +1,7 @@
-﻿using Eventures.App.Models;
+﻿using Eventures.App.Data;
+using Eventures.App.Domain;
+using Eventures.App.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -9,10 +12,18 @@ namespace Eventures.App.Controllers
 {
     public class EventureUsersController : Controller
     {
+        private readonly ApplicationDbContext context;
+        private readonly UserManager<EventuresUser> _userManager;
+
+        public EventureUsersController(ApplicationDbContext _context, UserManager<EventuresUser> userManager)
+        {
+            this.context = _context;
+            this._userManager = userManager;
+        }
 
         public async Task<IActionResult> Index()
         {
-            var users = this.userManager.Users
+            var users = _userManager.Users
                 .Select(u => new UserListingViewModel
                 {
                     Id = u.Id,
@@ -23,7 +34,59 @@ namespace Eventures.App.Controllers
 
                 }).ToList();
 
-            var adminIds = (await this.userManager.GetUser)
+            var adminIds = (await _userManager.GetUsersInRoleAsync("Administrator"))
+                .Select(a => a.Id).ToList();
+
+            foreach (var user in users)
+            {
+                user.IsAdmin = adminIds.Contains(user.Id);
+            }
+
+            var orderedUsers = users
+                .OrderByDescending(u => u.UserName);
+
+            return this.View(orderedUsers);
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Promote(string userId)
+        {
+            if (userId == null)
+            {
+                return this.RedirectToAction("Index");
+            }
+
+            var user = await _userManager.FindByIdAsync(userId);
+
+            if (user == null || await _userManager.IsInRoleAsync(user, "Administrator"))
+            {
+                return this.RedirectToAction("Index");
+            }
+
+            await _userManager.AddToRoleAsync(user, "Administrator");
+
+            return this.RedirectToAction("Index");
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Demote(string userId)
+        {
+            if (userId == null)
+            {
+                return this.RedirectToAction("Index");
+            }
+
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null || !await _userManager.IsInRoleAsync(user, "Administrator"))
+            {
+                return this.RedirectToAction("Index");
+            }
+
+            await _userManager.RemoveFromRoleAsync(user, "Administrator");
+
+            return this.RedirectToAction("Index");
         }
     }
 }
